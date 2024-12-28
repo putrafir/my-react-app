ab9b7ccafb2d3dff004ec5d88678c970f83e9402
putrafir <141028423+putrafir@users.noreply.github.com>
2024-12-29 00:00:13 +0700

    konfigurasi tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-  return <></>;
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <div>Hello World</div>
+    </div>
+  );
 }
 
 export default App;
