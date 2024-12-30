ddb47f57277c9750550645af3cfb585a1e997f19
putrafir <141028423+putrafir@users.noreply.github.com>
2024-12-30 23:16:54 +0700

    rendering list

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,24 +1,43 @@
 import { Button } from "flowbite-react";
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    image: "../../public/image/shoes-1.jpg",
+    name: "Sepatu Baru",
+    price: "Rp 1.000.000",
+    description:
+      "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+  },
+  {
+    id: 2,
+    image: "../../public/image/shoes-1.jpg",
+    name: "Sepatu Lama",
+    price: "Rp 2.000.000",
+    description: "Here are the biggest enterprise technology .",
+  },
+  {
+    id: 3,
+    image: "../../public/image/shoes-1.jpg",
+    name: "Sepatu Keren",
+    price: "Rp 6.000.000",
+    description: "Here are the biggest enterprise technology is something .",
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="../../public/image/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu" price="Rp 1.000.000">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </CardProduct.Body>
-      </CardProduct>
-
-      <CardProduct>
-        <CardProduct.Header image="../../public/image/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu" price="Rp 1.000.000">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </CardProduct.Body>
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -1,7 +1,7 @@
 const CardProduct = (props) => {
   const { children } = props;
   return (
-    <div className="max-w-sm mx-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm mx-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between ">
       {children}
     </div>
   );
@@ -17,31 +17,38 @@ const Header = (props) => {
 };
 
 const Body = (props) => {
-  const { children, title, price } = props;
+  const { children, name } = props;
   return (
-    <div className="p-5">
+    <div className="p-5 h-full">
       <a href="#">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {title}
+          {name}
         </h5>
       </a>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
         {children}
       </p>
-      <div className="flex  items-center justify-between">
-        <span className=" text-xl font-bold">{price}</span>
-        <button
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        >
-          Add To Cart
-        </button>
-      </div>
+    </div>
+  );
+};
+
+const Footer = (props) => {
+  const { price } = props;
+  return (
+    <div className="flex  items-center justify-between p-5">
+      <span className=" text-xl font-bold">{price}</span>
+      <button
+        type="button"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        Add To Cart
+      </button>
     </div>
   );
 };
 
 CardProduct.Header = Header;
 CardProduct.Body = Body;
+CardProduct.Footer = Footer;
 
 export default CardProduct;
